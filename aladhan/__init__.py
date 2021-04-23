eba43bd39efa92bc928505a75ac5416a50a20987
Takos <takos2210@gmail.com>
2021-04-23 17:48:00 +0200

    Bump version to 0.0.2

--- aladhan/__init__.py
+++ aladhan/__init__.py
@@ -11,11 +11,11 @@ VersionInfo = NamedTuple(
     "VersionInfo", major=int, minor=int, micro=int, releaselevel=str, serial=int
 )
 
-version_info = VersionInfo(major=0, minor=0, micro=1, releaselevel="", serial=0)
+version_info = VersionInfo(major=0, minor=0, micro=2, releaselevel="", serial=0)
 
 __title__ = "aladhan.py"
 __author__ = "HETHAT"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
 from .client import AsyncClient
