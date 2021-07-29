da4bae06b3904341e10f76109e7399cf1f496ef3
HETHAT <zhethat@gmail.com>
2021-07-29 23:12:57 +0100

    Fix docs ?

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath(".."))
 sys.path.append(os.path.abspath("extensions"))
+autodoc_mock_imports = ["pytz", "aiohttp", "requests", "typing_extensions"]
 
 # -- Project information -----------------------------------------------------
 
